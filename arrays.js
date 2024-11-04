444dc07905f3eafd4b7b3736355d266a4c4a01d1
meshariSalhouli <mesharialhouli12@gmail.com>
2024-11-04 11:26:39 +0300

    array task one

--- arrays.js
+++ arrays.js
@@ -64,7 +64,7 @@ function insertItemAtBeginning(groceries, item) {
 // Write a replaceFirstTwoItems function that replace the first two elements with 'ketchup' and 'chili'
 // and return the modified groceries array
 function replaceFirstTwoItems(groceries) {
-  groceries.splice(0, 2, "ketchup", "chili");
+  groceries.splice(0, 2, "Ketchup", "Chili");
   return groceries;
 }
 
